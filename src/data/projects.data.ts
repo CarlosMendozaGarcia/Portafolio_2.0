f98527072bd68c7548ebd5f81d571a66e5f507f3
CarlosMendozaGarcia <mendozagarcia@uninorte.edu.co>
2026-09-10 09:43:05 -0500

    fix (project.data): fixing data to adding real import for one image

--- src/data/projects.data.ts
+++ src/data/projects.data.ts
@@ -3,7 +3,7 @@ import Casamigos_1 from "../assets/Casamigos_1.png";
 import ConnectLatam from "../assets/Connect_Latam_VT.png";
 import ConnectLatam_1 from "../assets/Connect_Latam_VT_1.png"
 import PriceHive from "../assets/PriceHive.png";
-import CarPool from "../assets/CarPool.png";
+import CarPool from "../assets/Carpool.png";
 
 type Project = {
     title: string,
