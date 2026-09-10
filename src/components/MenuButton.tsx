569925c1b73b153ff867f6a91b19f709c20ed623
CarlosMendozaGarcia <mendozagarcia@uninorte.edu.co>
2026-09-10 10:47:27 -0500

    fix (CV): fixing on href CV to apply for redirection

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -49,7 +49,7 @@ export default function MenuButton({ children }: { children: React.ReactNode }) 
                 <a className="links-menu" href="#Experience">Experiencia</a>
                 <a className="links-menu" href="#Technologie">Tech Stack</a>
                 <a className="links-menu" href="#Projects">Proyectos</a>
-                <a className="links-menu button-primary active:button-primary-active transition-all duration-150" href="/public/Carlos_Mendoza_C_V_.pdf">Mi C.V. </a>
+                <a className="links-menu button-primary active:button-primary-active transition-all duration-150" href="/Carlos_Mendoza_C_V_.pdf">Mi C.V. </a>
             </ul>
         </div>
     </div>
